60f1156a37e737e4a548a0da8e6c0feedc20e3ed
uditiarora <uditiarora@gmail.com>
2020-02-18 16:43:00 +0530

    first lab done

---
+++ lab1/lab1.tcl
@@ -0,0 +1,117 @@
+
+ set ns [new Simulator]
+#define color for data flows
+$ns color 1 Blue
+$ns color 2 Red
+ #open tracefiles
+set tracefile1 [open out1.tr w]
+set winfile [open winfile w]
+$ns trace-all $tracefile1
+#open nam file
+set namfile [open out1.nam w]
+$ns namtrace-all $namfile
+#define the finish procedure
+proc finish {} {
+	global ns tracefile1 namfile
+	$ns flush-trace
+	close $tracefile1
+	close $namfile
+	exec nam out1.nam &
+	exit 0
+	} 
+#create six nodes
+set n0 [$ns node]
+set n1 [$ns node]
+set n2 [$ns node]
+set n3 [$ns node]
+set n4 [$ns node]
+set n5 [$ns node]
+$n1 color Red
+$n1 shape box
+
+
+$n0 color Blue
+$n0 shape box
+
+
+#create links between the nodes
+$ns duplex-link $n0 $n2 2Mb 10ms DropTail
+$ns duplex-link $n1 $n2 2Mb 10ms DropTail
+$ns simplex-link $n2 $n3 0.3Mb 100ms DropTail
+$ns simplex-link $n3 $n2 0.3Mb 100ms DropTail
+set lan [$ns newLan "$n3 $n4 $n5" 0.5Mb 40ms LL Queue/DropTail MAC/Csma/Cd Channel]
+
+#Give node position
+$ns duplex-link-op $n0 $n2 orient right-down
+$ns duplex-link-op $n1 $n2 orient right-up
+$ns simplex-link-op $n2 $n3 orient right
+$ns simplex-link-op $n3 $n2 orient left
+
+#set queue size of link(n2-n3) to 20
+$ns queue-limit $n2 $n3 20
+
+#setup TCP connection
+set tcp [new Agent/TCP/Newreno]
+$ns attach-agent $n0 $tcp
+set sink [new Agent/TCPSink/DelAck]
+$ns attach-agent $n4 $sink
+$ns connect $tcp $sink
+$tcp set fid_ 1
+$tcp set packet_size_ 552
+
+
+
+#setup TCP connection
+set tcp2 [new Agent/TCP/Newreno]
+$ns attach-agent $n1 $tcp2
+set sink [new Agent/TCPSink/DelAck]
+$ns attach-agent $n5 $sink
+$ns connect $tcp2 $sink
+$tcp2 set fid_ 2
+$tcp2 set packet_size_ 552
+
+
+#set ftp over tcp connection
+set ftp [new Application/FTP]
+$ftp attach-agent $tcp
+
+
+#set ftp over tcp connection
+set ftp2 [new Application/FTP]
+$ftp2 attach-agent $tcp2
+
+#setup a UDP connection
+set udp [new Agent/UDP]
+$ns attach-agent $n1 $udp
+set null [new Agent/Null]
+$ns attach-agent $n5 $null
+$ns connect $udp $null
+$udp set fid_ 2
+
+#setup a CBR over UDP connection
+set cbr [new Application/Traffic/CBR]
+$cbr attach-agent $udp
+$cbr set type_ CBR
+$cbr set packet_size_ 1000
+$cbr set rate_ 0.1Mb
+$cbr set random_ false
+
+#scheduling the events
+
+$ns at 0.1 "$ftp2 start"
+$ns at 0.1 "$ftp start"
+$ns at 124.0 "$ftp stop"
+$ns at 124.5 "$ftp2 stop"
+
+proc plotWindow {tcpSource file} {
+global ns
+set time 0.1
+set now [$ns now]
+set cwnd [$tcpSource set cwnd_]
+puts $file "$now $cwnd"
+ $ns at [expr $now+$time] "plotWindow $tcpSource $file"
+}
+$ns at 0.1 "plotWindow $tcp $winfile"
+ $ns at 125.0 "finish"
+ $ns run
+ 
